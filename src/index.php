a777ca7d05c846f4e784a13a43e88785f4da165b
Gavin Carberry <98050479+GavDotZip@users.noreply.github.com>
2023-05-25 19:03:18 +0100

    added code

--- src/index.php
+++ src/index.php
@@ -2,11 +2,22 @@
 <html>
 
 <head>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>
 </head>
 
 <body>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+                <form action="" method="post">
+                    
+                </form>
+
+            </div>
+        </div>
+    </div>
 
 </body>
 
