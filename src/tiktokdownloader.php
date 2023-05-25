a857fa80cb01ef6bf1e223aaba114aaeba12c199
Gavin Carberry <98050479+GavDotZip@users.noreply.github.com>
2023-05-25 18:58:26 +0100

    fixed formatting

--- src/tiktokdownloader.php
+++ src/tiktokdownloader.php
@@ -1,13 +1,15 @@
 <!-- Class taken from https://raw.githubusercontent.com/tiktokdownloadercvt/tiktokdownloader/main/tiktokdownloader.php -->
 <?php
+
 $store_locally = true;
 $downloadAddr = '"downloadAddr":"';
-function downloadVideo($video_url, $geturl = false)
-{
+function downloadVideo($video_url, $geturl = false) {
+
     $ch = curl_init();
     $headers = array(
         'Range: bytes=0-',
     );
+
     $options = array(
         CURLOPT_URL            => $video_url,
         CURLOPT_RETURNTRANSFER => true,
@@ -19,7 +21,7 @@ function downloadVideo($video_url, $geturl = false)
         CURLOPT_ENCODING       => "utf-8",
         CURLOPT_AUTOREFERER    => true,
         CURLOPT_COOKIEJAR      => 'cookie.txt',
-	CURLOPT_COOKIEFILE     => 'cookie.txt',
+	    CURLOPT_COOKIEFILE     => 'cookie.txt',
         CURLOPT_REFERER        => 'https://www.tiktok.com/',
         CURLOPT_CONNECTTIMEOUT => 30,
         CURLOPT_SSL_VERIFYHOST => false,
@@ -27,16 +29,19 @@ function downloadVideo($video_url, $geturl = false)
         CURLOPT_TIMEOUT        => 30,
         CURLOPT_MAXREDIRS      => 10,
     );
+
     curl_setopt_array( $ch, $options );
     if (defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4')) {
       curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
     }
+
     $data = curl_exec($ch);
     $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     if ($geturl === true)
     {
         return curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
     }
+
     curl_close($ch);
     $filename = "videos/" . uniqid() . ".mp3";
     $d = fopen($filename, "w");
@@ -71,7 +76,7 @@ function getContent($url, $geturl = false)
         CURLOPT_ENCODING       => "utf-8",
         CURLOPT_AUTOREFERER    => false,
         CURLOPT_COOKIEJAR      => 'cookie.txt',
-	CURLOPT_COOKIEFILE     => 'cookie.txt',
+	    CURLOPT_COOKIEFILE     => 'cookie.txt',
         CURLOPT_REFERER        => 'https://www.tiktok.com/',
         CURLOPT_CONNECTTIMEOUT => 30,
         CURLOPT_SSL_VERIFYHOST => false,
@@ -79,16 +84,19 @@ function getContent($url, $geturl = false)
         CURLOPT_TIMEOUT        => 30,
         CURLOPT_MAXREDIRS      => 10,
     );
+
     curl_setopt_array( $ch, $options );
     if (defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4')) {
       curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
     }
+
     $data = curl_exec($ch);
     $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     if ($geturl === true)
     {
         return curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
     }
+
     curl_close($ch);
     return strval($data);
   }
@@ -113,7 +121,7 @@ function getContent($url, $geturl = false)
         CURLOPT_ENCODING       => "utf-8",
         CURLOPT_AUTOREFERER    => false,
         CURLOPT_COOKIEJAR      => 'cookie.txt',
-	CURLOPT_COOKIEFILE     => 'cookie.txt',
+	    CURLOPT_COOKIEFILE     => 'cookie.txt',
         CURLOPT_REFERER        => 'https://www.tiktok.com/',
         CURLOPT_CONNECTTIMEOUT => 30,
         CURLOPT_SSL_VERIFYHOST => false,
@@ -121,10 +129,12 @@ function getContent($url, $geturl = false)
         CURLOPT_TIMEOUT        => 30,
         CURLOPT_MAXREDIRS      => 10,
     );
+
     curl_setopt_array( $ch, $options );
     if (defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4')) {
       curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
     }
+    
     $data = curl_exec($ch);
     $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
